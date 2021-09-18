9ae10af698533178c61d9ed9a1d832aa3949a882
hackerrank <hfgoff@wisc.edu>
2021-09-18 18:23:34 -0500

    my commits

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 import { Col, Row, Container, Button } from "react-bootstrap";
 import "./Content.css";
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth, firestore } from '../firebaseStuff'
-var nodemailer = require('nodemailer');
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../firebaseStuff";
+var nodemailer = require("nodemailer");
 
 function Content(props) {
-
   const [user] = useAuthState(auth);
 
-  let appEmail = 'mentally.app.team@gmail.com';
-  const [password, setPassword] = useState('');
+  let appEmail = "mentally.app.team@gmail.com";
+  const [password, setPassword] = useState("");
 
   // useEffect(() => {
   //   firestore.collection('superSecretPassword').onSnapshot((snapshot) => {
@@ -48,7 +47,6 @@ function Content(props) {
   //     }
   //   });
   // }
-  
 
   // auth: (...)
   // displayName: (...)
@@ -66,30 +64,17 @@ function Content(props) {
 
   return (
     <Container fluid>
-<<<<<<< HEAD
       <Row className=""></Row>
-=======
-      <Row>
-        <Col className="col-sm-6 ">
-          <h1 className="addContactText"> Welcome Back, {user.displayName}!</h1>
-        </Col>
-      </Row>
->>>>>>> 76c775434d2b7c0340d68df47f0f78fac23fcf78
       <Row>
         <Col className="col-sm-6 ">
           <h1 className="addContactText"> Add Contacts</h1>
         </Col>
         <Col className="col-sm-6 ">
-<<<<<<< HEAD
           <button type="submit" class="btn btn-primary">
             add contact
           </button>
-=======
-          {/* <Button onClick={sendEmail}>Send Email</Button> */}
->>>>>>> 76c775434d2b7c0340d68df47f0f78fac23fcf78
         </Col>
       </Row>
-
     </Container>
   );
 }
