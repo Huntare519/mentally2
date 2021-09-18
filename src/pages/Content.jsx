74eb29ba87bd95e451f78c51591d1e93aa12544f
Bailey Conradt <baileyconradt@gmail.com>
2021-09-18 18:22:10 -0500

    contaact page in progress

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -27,27 +27,19 @@ function Content(props) {
 
   return (
     <Container fluid>
-<<<<<<< HEAD
-      <Row className=""></Row>
-=======
       <Row>
         <Col className="col-sm-6 ">
           <h1 className="addContactText"> Welcome Back, {user.displayName}!</h1>
         </Col>
       </Row>
->>>>>>> 76c775434d2b7c0340d68df47f0f78fac23fcf78
       <Row>
         <Col className="col-sm-6 ">
           <h1 className="addContactText"> Add Contacts</h1>
         </Col>
         <Col className="col-sm-6 ">
-<<<<<<< HEAD
           <button type="submit" class="btn btn-primary">
             add contact
           </button>
-=======
-          {/* <Button onClick={sendEmail}>Send Email</Button> */}
->>>>>>> 76c775434d2b7c0340d68df47f0f78fac23fcf78
         </Col>
       </Row>
 
