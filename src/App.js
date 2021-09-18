4cd8c7185439fbc0c3904e58ea53d2c6f822b4f7
hackerrank <hfgoff@wisc.edu>
2021-09-18 12:32:57 -0500

    routing done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//pages
+import Content from "./pages/Content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Router>
+        <Route path="/" exact>
+          <p> Homepage </p>
+        </Route>
+        <Route path="/login" render={() => <Content />} />
+        <Route path="/signup" render={() => <Content />} />
+        <Route path="/content" render={() => <Content />} />
+        <Route path="/myaccount" render={() => <Content />} />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div>
+      <p> Content Page</p>
+    </div>
+  );
+}
+
+export default Content;
