cd5968d531ef3d590d12dcfda31e2757a535f700
hackerrank <hfgoff@wisc.edu>
2021-09-18 14:21:30 -0500

    updated UI/UX homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 
+import { Col, Row, Container } from "react-bootstrap";
+
+import logo from "../assets/mentally_logo.png";
+import "./Homepage.css";
+
+import { Button } from "react-bootstrap";
+
 function Content() {
   return (
-    <div className="">
-      <h3 className="branding"> Mentally </h3>
-    </div>
+    <Container fluid>
+      <Row className="first-row">
+        <Col>
+          <h3 className="branding"> </h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <img class="col-6 img-logo " src={logo} alt="mentally logo"></img>
+        </Col>
+        <Col class="col-6">
+          <h3 className="branding text center"> Mentally </h3>
+        </Col>
+      </Row>
+      <Row className="row-2">
+        <Col className="col-sm-12 col-lg-6 button-align">
+          <button type="button" className="btn btn-light">
+            {" "}
+            login
+          </button>
+        </Col>
+        <Col className="col-sm-12 col-lg-6 button-align">
+          <button type="button" className="btn btn-light">
+            {" "}
+            sign up
+          </button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
